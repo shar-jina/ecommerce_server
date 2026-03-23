baf25fe352bd5cd278527734cf91a96bc7022026
shar-jina <sharjinaraseena@gmail.com>
2026-03-23 14:42:27 +0530

    db

--- config/db.js
+++ config/db.js
@@ -1,11 +1,23 @@
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "ecommerce_db",
+//   password: "shar123",
+//   port: 5000,
+// });
+
+// module.exports = pool;
+
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "ecommerce_db",
-  password: "shar123",
-  port: 5000,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
