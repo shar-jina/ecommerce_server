3a5dc9ba58294f418b7968ff1840fee118bcab97
shar-jina <sharjinaraseena@gmail.com>
2026-03-24 11:26:32 +0530

    payment

--- routes/settingsRoutes.js
+++ routes/settingsRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const settingsController = require('../controllers/settingscontroller');
 const verifytoken = require('../middleware/jwtmiddleware');
-const allowRoles = require("../middleware/roleMiddleware");
+const allowRoles = require("../middleware/allowRoles");
 
 // Public can read the settings (needed for checkout)
 router.get("/payment", settingsController.getPaymentSettings);
