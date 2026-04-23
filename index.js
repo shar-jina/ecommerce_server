840050d8492c4f4924c0070bfd867a259b3a24bf
shar-jina <sharjinaraseena@gmail.com>
2026-04-23 17:50:32 +0530

    add

--- index.js
+++ index.js
@@ -149,7 +149,8 @@ app.use(cors({
   origin: [
     process.env.FRONTEND_URL, 
     "http://localhost:5173", 
-    "https://ecommerce-qzvk-git-main-sharjinas-projects.vercel.app/"
+    "https://ecommerce-qzvk-git-main-sharjinas-projects.vercel.app",
+    "https://ecommerce-fjnw.vercel.app"
   ].filter(Boolean), 
   credentials: true 
 }));
