63f74c16cc33dc7912a475e9198cdf66668be9dd
shar-jina <sharjinaraseena@gmail.com>
2026-04-23 16:30:28 +0530

    add

--- index.js
+++ index.js
@@ -178,12 +178,10 @@ app.get("/", (req, res) => {
   res.status(200).send("Ecommerce Backend Running");
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;
 
